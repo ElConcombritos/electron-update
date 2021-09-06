3933072d1dfb8567c4abb73035974bb57d777786
acreoo <acreoo@acreoo-NC03U>
2021-09-06 16:56:33 +0200

    new version

--- main.js
+++ main.js
@@ -13,8 +13,41 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
-	autoUpdater.checkForUpdatesAndNotify()
+    try {
+        const info = await autoUpdater.checkForUpdatesAndNotify();
+        logger.info('checkForUpdatesAndNotify');
+        logger.info(JSON.stringify(info));
 
+        autoUpdater.on('update-downloaded', info => {
+            const quitAndInstalled = autoUpdater.quitAndInstall();
+            logger.info('quitAndInstalled');
+            logger.info(quitAndInstalled);
+        });
+
+        autoUpdater.on('update-available', arg => {
+            logger.info('update-available');
+            logger.info(arg);
+        });
+
+        autoUpdater.on('update-not-available', arg => {
+            logger.info('update-not-available');
+            logger.info(arg);
+        });
+
+        autoUpdater.on('download-progress', arg => {
+            logger.info('download-progress');
+            logger.info(arg);
+        });
+
+        autoUpdater.on('error', error => {
+            logger.info('error');
+            logger.info(error.message);
+            logger.info(error.stack);
+        });
+    }
+    catch (error) {
+        logger.info('autoupdate failed');
+    }
 
   createWindow()
 })
