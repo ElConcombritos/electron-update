07039042ebebb4c8a5c04a6c65b8819c3dc56854
acreoo <acreoo@acreoo-NC03U>
2021-09-09 14:21:43 +0200

    new version

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const {autoUpdater} = require("electron-updater");
+const fs = require("fs")
+const path = require("path")
+
+var user = ""
+var pass = ""
+var proxyString = ""
+
+let rawdata = fs.readFile("/home/acreoo/info_proxy.json",'utf8',(err, data) => {
+	if (err) {
+		console.log(err)
+		return	
+	}
+	console.log(JSON.parse(data))
+	user = JSON.parse(data).user
+	pass = JSON.parse(data).pass
+	proxyString = "http://"+user+":"+pass+"@192.168.0.165:3128"
+
+})
 
 	
 
@@ -17,7 +35,7 @@ async function checkforupdates() {
         const info = await autoUpdater.checkForUpdatesAndNotify();
 
 		autoUpdater.netSession.setProxy({
-			proxyRules: "pouetpouet",
+			proxyRules: proxyString,
 		});
         autoUpdater.on('update-downloaded', info => {
             const quitAndInstalled = autoUpdater.quitAndInstall();
